73ed6d3b2e2e898d9cd1f111ae7b5581d4028fbc
samia429 <samsokhna429@gmail.com>
2024-11-23 18:32:32 +0000

    Add files via upload

---
+++ App.py
@@ -0,0 +1,10 @@
+import streamlit as st 
+
+st.title("Bienvenue dans Design App")
+
+st.markdown (""" 
+             ### - Accueil 
+             ### - Définition et utilité du Design
+             """)
+
+
